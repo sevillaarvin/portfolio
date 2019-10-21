c048b1e5e2d9241f549d8979fd36acf573a9561e
Arvin Sevilla <sevillaarvin@gmail.com>
2019-10-22 00:09:25 +0800

    Refactor auto format webpack config file

--- webpack.config.js
+++ webpack.config.js
@@ -6,77 +6,77 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin")
 const PurgecssPlugin = require("purgecss-webpack-plugin")
 
 module.exports = {
-  // default:
-  // entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, "public")
-  },
+    // default:
+    // entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, "public")
+    },
     optimization: {
-      splitChunks: {
-        cacheGroups: {
-          styles: {
-            name: "styles",
-            test: /\.css$/,
-            chunks: "all",
-            enforce: true,
-          },
+        splitChunks: {
+            cacheGroups: {
+                styles: {
+                    name: "styles",
+                    test: /\.css$/,
+                    chunks: "all",
+                    enforce: true,
+                },
+            },
         },
-      },
     },
     module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          "css-loader",
-          {
-            loader: "postcss-loader",
-            options: {
-              ident: "postcss",
-              plugins: [
-                require("tailwindcss"),
-                require("autoprefixer"),
-              ],
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    "css-loader",
+                    {
+                        loader: "postcss-loader",
+                        options: {
+                            ident: "postcss",
+                            plugins: [
+                                require("tailwindcss"),
+                                require("autoprefixer"),
+                            ],
+                        },
+                    },
+                ],
             },
-          },
-        ],
-      },
-      {
-        test: /\.(png|jpe?g|gif)$/,
-        use: [
-          {
-            loader: "file-loader",
-            options: {
-              name: '[path][name].[ext]',
+            {
+                test: /\.(png|jpe?g|gif)$/,
+                use: [
+                    {
+                        loader: "file-loader",
+                        options: {
+                            name: '[path][name].[ext]',
+                        },
+                    },
+                ],
             },
-          },
-        ],
-      },
-    ]
-  },
-  plugins: [
-    new BrowserSyncPlugin({
-      host: "localhost",
-      port: 3000,
-      proxy: "http://localhost:3100"
-    }, {
-      reload: false
-    }),
-    new HtmlWebpackPlugin({
-      name: "Arvin Sevilla",
-      title: "Software Engineer",
-      template: "src/index.html"
-    }),
-    new MiniCssExtractPlugin({
-      filename: "[name].css",
-      chunkFilename: "[id].css",
-    }),
-    new PurgecssPlugin({
-      paths: glob.sync(`${path.resolve(__dirname, "src")}/**/*`, {nodir: true}),
-    }),
-  ],
-  devServer: {
-    port: 3100
-  },
+        ]
+    },
+    plugins: [
+        new BrowserSyncPlugin({
+            host: "localhost",
+            port: 3000,
+            proxy: "http://localhost:3100"
+        }, {
+            reload: false
+        }),
+        new HtmlWebpackPlugin({
+            name: "Arvin Sevilla",
+            title: "Software Engineer",
+            template: "src/index.html"
+        }),
+        new MiniCssExtractPlugin({
+            filename: "[name].css",
+            chunkFilename: "[id].css",
+        }),
+        new PurgecssPlugin({
+            paths: glob.sync(`${path.resolve(__dirname, "src")}/**/*`, {nodir: true}),
+        }),
+    ],
+    devServer: {
+        port: 3100
+    },
 }
